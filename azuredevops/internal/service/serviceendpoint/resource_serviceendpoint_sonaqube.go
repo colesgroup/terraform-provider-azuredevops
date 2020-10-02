7e73fc1f4568c525ac1a8915d977e2da0c8a39f6
Bill Birch <bill.birch@coles.com.au>
2020-10-02 15:04:26 +1000

    #195 service endpoint sonarqube - remove 'all_pipelines' ; hash 'token' in state file

--- azuredevops/internal/service/serviceendpoint/resource_serviceendpoint_sonaqube.go
+++ azuredevops/internal/service/serviceendpoint/resource_serviceendpoint_sonaqube.go
@@ -5,6 +5,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"github.com/microsoft/azure-devops-go-api/azuredevops/serviceendpoint"
 	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/internal/utils/converter"
+	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/internal/utils/tfhelper"
 )
 
 // ResourceServiceEndpointSonarQube schema and implementation for SonarQube service endpoint resource
@@ -19,18 +20,16 @@ func ResourceServiceEndpointSonarQube() *schema.Resource {
 	}
 
 	r.Schema["token"] = &schema.Schema{
-		Type:         schema.TypeString,
-		Required:     true,
-		ValidateFunc: validation.StringIsNotWhiteSpace,
-		Description:  "Authentication Token generated through SonarQube (go to My Account > Security > Generate Tokens)",
+		Type:             schema.TypeString,
+		Optional:         true,
+		Sensitive:        true,
+		DiffSuppressFunc: tfhelper.DiffFuncSuppressSecretChanged,
+		ValidateFunc:     validation.StringIsNotWhiteSpace,
+		Description:      "Authentication Token generated through SonarQube (go to My Account > Security > Generate Tokens)",
 	}
-
-	r.Schema["all_pipelines"] = &schema.Schema{
-		Type:        schema.TypeBool,
-		Optional:    true,
-		Default:     true,
-		Description: "Grant access permission to all pipelines",
-	}
+	// Add a spot in the schema to store the token secretly
+	stSecretHashKey, stSecretHashSchema := tfhelper.GenerateSecreteMemoSchema("token")
+	r.Schema[stSecretHashKey] = stSecretHashSchema
 
 	return r
 }
@@ -45,7 +44,6 @@ func expandServiceEndpointSonarQube(d *schema.ResourceData) (*serviceendpoint.Se
 			"username": d.Get("token").(string),
 		},
 	}
-	serviceEndpoint.IsShared = converter.Bool(d.Get("all_pipelines").(bool))
 	serviceEndpoint.Type = converter.String("sonarqube")
 	serviceEndpoint.Url = converter.String(d.Get("url").(string))
 	return serviceEndpoint, projectID, nil
@@ -55,52 +53,9 @@ func expandServiceEndpointSonarQube(d *schema.ResourceData) (*serviceendpoint.Se
 func flattenServiceEndpointSonarQube(d *schema.ResourceData, serviceEndpoint *serviceendpoint.ServiceEndpoint, projectID *string) {
 
 	doBaseFlattening(d, serviceEndpoint, projectID)
+
+	tfhelper.HelpFlattenSecret(d, "token")
+
 	d.Set("url", *serviceEndpoint.Url)
-	d.Set("all_pipelines", *serviceEndpoint.IsShared)
 	d.Set("token", (*serviceEndpoint.Authorization.Parameters)["username"])
 }
-
-// Example Terraform:
-//
-// resource "azuredevops_serviceendpoint_sonarqube" "example" {
-// 		project_id = azuredevops_project.project.id
-// 		url   = "https://sonarqube.instance.com"
-// 		token = "xxx"
-// 		service_endpoint_name = "example-sonarqube-1"
-// 		description           = "example-1"
-//   }
-
-// CLI: https://docs.microsoft.com/en-us/cli/azure/ext/azure-devops/devops/service-endpoint?view=azure-cli-latest#ext_azure_devops_az_devops_service_endpoint_create
-// How to obtain Config: https://docs.microsoft.com/en-us/azure/devops/cli/service-endpoint?view=azure-devops#create-service-endpoint-using-configuration-file
-//
-// Example API Request (via Chrome developer tools)
-// {
-// 	"administratorsGroup": null,
-// 	"authorization": {
-// 		"parameters": {
-// 			"username": "redactedredactedredactedredactedredactedredacted"
-// 		},
-// 		"scheme": "UsernamePassword"
-// 	},
-// 	"createdBy": null,
-// 	"data": {},
-// 	"description": "My Big Fat Description",
-// 	"groupScopeId": null,
-// 	"isShared": false,
-// 	"name": "My Big Fat Service Connection Name",
-// 	"operationStatus": null,
-// 	"owner": "library",
-// 	"readersGroup": null,
-// 	"serviceEndpointProjectReferences": [
-// 	{
-// 		"description": "My Big Fat Description",
-// 		"name": "My Big Fat Service Connection Name",
-// 		"projectReference": {
-// 		"id": "deadbeef-cafe-cafe-deadbeef-deadbeef",
-// 		"name": "My Big Fat Project"
-// 		}
-// 	}
-// 	],
-// 	"type": "sonarqube",
-// 	"url": "http://example.com"
-// }
